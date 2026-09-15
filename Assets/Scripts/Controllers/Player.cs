54baa8659bfa34385de9ceda6b2c6b802da8f902
Clark <osborneclark5@gmail.com>
2026-09-15 10:22:51 -0400

    Task 1: Challenge 1/2 Completed

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,17 +9,27 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float numberOfBombs;
+    public Vector2 bombTrailSpacing;
+
 
     void Update()
-    {
+    {   
+        Vector3 playersTransform = transform.position;
+
         if(Keyboard.current.bKey.wasPressedThisFrame)
         {
-            SpawnBombAtOffset(transform.position + Vector3.up);
+            SpawnBombAtOffset(playersTransform + Vector3.up);
+        }
+
+        if(Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            SpawnBombTrail(playersTransform);
         }
 
         if(Keyboard.current.wKey.wasPressedThisFrame)
         {
-            Vector2 direction = (enemyTransform.position - transform.position);
+            Vector2 direction = (enemyTransform.position - playersTransform);
             //Vector2.Distance(enemyTransform.position, transform.position);
             warpDrive(direction);
             
@@ -29,7 +39,17 @@ public class Player : MonoBehaviour
     void SpawnBombAtOffset(Vector3 inOffset)
     {
         GameObject bomb = Instantiate(bombPrefab);
-        bombPrefab.transform.position = inOffset;
+        bomb.transform.position = inOffset;
+    }
+
+    void SpawnBombTrail(Vector3 playerPostion)
+    {
+        for(int i = 1; i < numberOfBombs + 1; i++)
+        {
+            GameObject bomb = Instantiate(bombPrefab);
+            bomb.transform.position = playerPostion - (Vector3) (bombTrailSpacing * i);
+            Debug.Log(bombPrefab.transform.position);
+        }
     }
 
     void warpDrive(Vector3 direction)
