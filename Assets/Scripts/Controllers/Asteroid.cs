5893ec67da22a29ea0bbbff14adf749eac59f777
osborncl <osborncl@OA-S318-18>
2026-09-23 10:01:44 -0400

    Started Asteriod task, had to stop

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -7,14 +7,28 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    public Vector2 arivalLocation;
+    public Vector2 movementDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        arivalLocation = new Vector2(transform.position.x + Random.Range(-maxFloatDistance, maxFloatDistance), transform.position.y + Random.Range(-maxFloatDistance, maxFloatDistance));
+        //movementDirection = Vector2.Distance(arivalLocation, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //transform.position += Vector3.Normalize(movementDirection);
+        //movementDirection = arivalLocation * moveSpeed * Time.deltaTime;
+        //transform.position = (Vector2) transform.position + movementDirection * Time.deltaTime;
+
+        //transform.position += Vector3.Lerp(transform.position, arivalLocation, moveSpeed) * Time.deltaTime;
+
+        if (transform.position.magnitude < maxFloatDistance)
+        {
+            arivalLocation = new Vector2(transform.position.x + maxFloatDistance, transform.position.y + maxFloatDistance);
+        }
     }
 }
