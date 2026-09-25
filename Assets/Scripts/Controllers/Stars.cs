017ce4844116e013c3bd4e6c7b1c021fa7c49263
Clark <osborneclark5@gmail.com>
2026-09-24 21:32:19 -0400

    Task 3 – Constellation

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -6,19 +6,32 @@ public class Stars : MonoBehaviour
 {
     public List<Transform> starTransforms;
     public float drawingTime;
-    Vector2 currentPos;
+    public Vector2 starPos;
+    public int i;
+    public Vector2 movementDirection;
+
+    void Start()
+    {
+        starPos = starTransforms[0].position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 starPos = starTransforms[0].position;
-        Debug.DrawLine(starPos, starTransforms[0].position, Color.white, 1f);
-        starPos = Vector3.Lerp(starTransforms[1].position, starPos, 1f) * Time.deltaTime;
-    }
+        Vector2 movementDirection = (starTransforms[i].position - (Vector3) starPos).normalized * Time.deltaTime;
+        starPos += movementDirection;
 
-    void Start()
-    {
-        //DrawConstellation();
+        if(Vector2.Distance(starPos, starTransforms[i].position) <= 0.1f)
+        {
+            i++;
+            if(i == starTransforms.Count)
+            {
+                i = 0;
+            }
+
+        }
+
+        Debug.DrawLine(starPos, starTransforms[i-1].position, Color.white);
     }
 
     void DrawConstellation()
